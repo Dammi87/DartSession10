12b94efcca5a9fec507fdbe6ed3c27eb399198ad
Adam Hart Rúnarsson Fjeldsted <adamhrf@ntv.is>
2021-11-03 18:38:58 +0000

    Updated dart version

--- test/nested_loops_and_lists_test.dart
+++
@@ -1,8 +0,0 @@
-import 'package:nested_loops_and_lists/nested_loops_and_lists.dart';
-import 'package:test/test.dart';
-
-void main() {
-  test('calculate', () {
-    expect(calculate(), 42);
-  });
-}
